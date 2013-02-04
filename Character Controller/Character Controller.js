1bf7e72f36bf3eafdce651dee357b0f4ffe0ad96
Nguyen Phi Long <Zeka304@yahoo.com>
2013-02-05 03:20:41 +0800

    Added animation, jump logic. Still need to fix horizontal movement and jumping animation (loop once)

--- Character Controller/Character Controller.js
+++ Character Controller/Character Controller.js
@@ -1,6 +1,10 @@
 
+private var controller;
+
+var isGrounded : boolean;
+
 // Helper class that contains all movement related data
-class CharacterMovementContainer {
+class CharacterHorizontalMovementConfiguration {
 	// Current walking speed
 	var walkSpeed = 0.0;
 	
@@ -18,55 +22,115 @@ class CharacterMovementContainer {
 	
 	// Movement checking
 	var isMoving = false;
+	
 }
 
-var movement : CharacterMovementContainer;
+var groundMovement : CharacterHorizontalMovementConfiguration;
+
+class CharacterVerticalMovementConfiguration {
+	// Horizontal speed
+	var airSpeed = 0.0;
+	
+	// Gravity
+	var gravity = -10.0;
+	
+	// Initial horizontal (jumping) speed
+	var initialSpeed = 10.0;
+	
+	// Terminal falling speed
+	var terminalSpeed = -10.0;
+	
+	// Horizontal deceleration while airborne
+	var deceleration = 1.0;
+}
+
+var airMovement : CharacterVerticalMovementConfiguration;
+
+class AnimationConfiguration {
+	// Walking animation modifier
+	var walkingSpeedModifier = 1.0;
+	
+}
+
+var animationCfg : AnimationConfiguration;
 
 function Start () {
-
+	var run = animation["run"];
+	run.speed *= animationCfg.walkingSpeedModifier;
+	controller = GetComponent(CharacterController);
 }
 
 // Handles ground movement
-function UpdateMovement() {
+function UpdateGroundMovement() {
 	var h = Input.GetAxisRaw ("Horizontal");
 	
-	movement.isMoving = Mathf.Abs (h) > 0.1;
+	groundMovement.isMoving = Mathf.Abs (h) > 0.1;
 	
-	if (movement.isMoving){
-		movement.direction = Vector3 (h, 0, 0);
-		movement.walkSpeed += movement.acceleration * Time.deltaTime;
-		movement.walkSpeed = Mathf.Min(movement.walkSpeed, movement.maxWalkSpeed);
+	if (groundMovement.isMoving){
+		groundMovement.direction = Vector3 (h, 0, 0);
+		groundMovement.walkSpeed += groundMovement.acceleration * Time.deltaTime;
+		groundMovement.walkSpeed = Mathf.Min(groundMovement.walkSpeed, groundMovement.maxWalkSpeed);
 	}else {
-		movement.direction = Vector3.zero;
-		movement.walkSpeed -= movement.deceleration * Time.deltaTime;
-		movement.walkSpeed = Mathf.Max(movement.walkSpeed, 0);
+		groundMovement.direction = Vector3.zero;
+		if (controller.isGrounded)
+			groundMovement.walkSpeed -= groundMovement.deceleration * Time.deltaTime;
+		else
+			groundMovement.walkSpeed -= airMovement.deceleration * Time.deltaTime;
+		groundMovement.walkSpeed = Mathf.Max(groundMovement.walkSpeed, 0);
 	}
 		
+}
+
+function UpdateAirMovement() {
+	if (controller.isGrounded && Input.GetButtonDown("Jump"))
+		airMovement.airSpeed += airMovement.initialSpeed;
+		
+	// Apply gravity if character is not grounded
+	if (!controller.isGrounded)
+		airMovement.airSpeed += airMovement.gravity * Time.deltaTime;
+	else
+		airMovement.airSpeed = Mathf.Max(-1.0, airMovement.airSpeed);
+		
+	airMovement.airSpeed = Mathf.Max(airMovement.terminalSpeed, airMovement.airSpeed);
 }
 
 function UpdateAnimation() {
-	/*if (movement.isMoving)
-		animation.CrossFade("run");
-	else
-		animation.CrossFade("idle", 0.5);*/
+	if (!controller.isGrounded){
+		if (airMovement.airSpeed >= 0)
+			animation.CrossFade("jump");
+		else
+			animation.CrossFade("ledgeFall");
+		
+	} else {
+		if (groundMovement.isMoving)
+			animation.CrossFade("run");
+		else
+			animation.CrossFade("idle", 1.0);
+	}
 }
 
 function Update () {
-	UpdateMovement();
+	var displacement = Vector3.zero;
 	
-	// Update movement
-	if (movement.isMoving) {
-		/*Debug.Log(movement.direction);
-		Debug.Log(movement.walkSpeed);
-		Debug.Log(Time.deltaTime);*/
-		var displacement = movement.direction * movement.walkSpeed * Time.deltaTime;
-		Debug.Log(displacement);
-		transform.position += displacement;
-	}
-		
+	UpdateGroundMovement();
+	
+	UpdateAirMovement();
+	
+	displacement += groundMovement.direction * groundMovement.walkSpeed * Time.deltaTime;
+	displacement += Vector3(0, airMovement.airSpeed, 0) * Time.deltaTime;
+	
+	isGrounded = controller.isGrounded;
+	
+	// Gravity
+	// displacement += airMovement.gravity * Time.deltaTime;
+	
+	// Apply movement if there is one
+	//Debug.Log(displacement);
+	controller.Move(displacement);
+				
 	// Update facing direction	
-	if (movement.direction != Vector3.zero)
-		transform.rotation = Quaternion.LookRotation (movement.direction);
+	if (groundMovement.direction != Vector3.zero)
+		transform.rotation = Quaternion.LookRotation (groundMovement.direction);
 		
 	UpdateAnimation();
 }
